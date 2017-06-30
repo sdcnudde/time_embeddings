7dbc609bf7960319ef309e6cee716193f6dc6193
sdcnudde <sofie.decnudde@gmail.com>
2017-06-30 09:00:21 +0100

    Update word2vec.py

--- src/python/word2vec.py
+++ src/python/word2vec.py
@@ -25,20 +25,25 @@ class MySentences(object):
 sentences = MySentences('~/Documents/customer_sentences2.txt')
 dates = MySentences('~/Documents/dates2_sentences.csv')
 
-start_time = time.time()
-dim = 64
-wdw = 20
+dims = [2, 4, 8, 16, 32, 64, 128, 256, 512]
+wdws = [1, 2, 5, 10, 20]
 iter = 5
-model = gensim.models.Word2Vec(sentences, sample=0, size=dim, window=wdw, min_count=1, sg=1, seed=42, hs=0, negative=5, iter=iter)
-#model = gensim_cust.models.Word2Vec(sentences, dates, sample=0, size=dim, window=wdw, min_count=1, sg=1, seed=42, hs=0, negative=5, iter=iter)
+skipg = 1
 
-stop_time = time.time()
-print stop_time - start_time
+for i in range(0, len(dims)):
+    for j in range(0, len(wdws)):
+        start_time = time.time()
+        dim = dims[i]
+        wdw = wdws[j]
+        model = gensim.models.Word2Vec(sentences, sample=0, size=dim, window=wdw, min_count=1, sg=skipg, seed=42, hs=0, negative=5, iter=iter)
 
-# Save output weights
-# numpy.savetxt('w2v_outputweights.txt', model.syn1neg)
-word_vectors = model.wv
+        stop_time = time.time()
+        print stop_time - start_time
 
-# Save input weights
-str_fname = '_dim_' + str(dim) + '_wdw_' + str(wdw) + '_iter_' + str(iter)
-word_vectors.save_word2vec_format('~/Documents/w2v_output' + str_fname + '.txt')
+        # Save output weights
+        # numpy.savetxt('w2v_outputweights.txt', model.syn1neg)
+        word_vectors = model.wv
+
+        # Save input weights
+        str_fname = '_dim_' + str(dim) + '_wdw_' + str(wdw) + '_iter_' + str(iter) + '_skip_' + str(skipg)
+        word_vectors.save_word2vec_format('~/Documents/w2v_output' + str_fname + '.txt')
